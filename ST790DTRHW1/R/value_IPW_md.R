e325597897873f8f361fd283fae2753cec536b9f
zhuolinsong@g.ucla.edu <zong7@ncsu.edu>
2020-10-21 12:46:21 -0400

    hw3

--- ST790DTRHW1/R/value_IPW_md.R
+++ ST790DTRHW1/R/value_IPW_md.R
@@ -3,15 +3,15 @@
 #   K>1 decision points
 #
 # ASSUMPTIONS
-#   each tx is binary coded as {0,1}
+#   each of the tx is denoted by Ak k=(1,...,K) and is binary coded as {0,1}
 #
 # INPUTS
 #  moPS   : list of modeling objects for propensity score regressions
-#           kth element corresponds to kth decision point
+#           must be logistic models; kth element corresponds to kth decision
+#           point
 #  data   : data.frame containing all covariates and txs
-#           *** each tx must be coded 0/1 ***
+#           txs must be named 'Ak' (k=1,...,K) and coded 0/1
 #  y      : outcome of interest
-#  txName : vector of tx names (each tx must be coded as 0/1)
 #  regime : matrix of 0/1 containing recommended txs
 #           kth column corresponds to kth decision point
 #
@@ -20,7 +20,7 @@
 #     fitPS : list of value objects returned by propensity score regression
 #  valueHat : estimated value
 #------------------------------------------------------------------------------#
-value_IPW_md <- function(moPS, data, y, txName, regime) {
+value_IPW_md <- function(moPS, data, y, regime) {
 
   # number of decision points
   K <- length(x = moPS)
@@ -28,61 +28,36 @@ value_IPW_md <- function(moPS, data, y, txName, regime) {
   # number of patients in data set
   n <- nrow(x = data)
 
-  #### Propensity Score Regressions
+  # tx names in data set
+  nms <- paste0('A',1L:K)
 
-  piStep <- function(moPS, data, y, regime) {
-    # fit propensity score model
-    fitObj <- modelObj::fit(object = moPS, data = data, response = y)
-
-    # predict propensity score
-    pk <- modelObj::predict(object = fitObj, newdata = data)
-    if (is.matrix(x = pk)) pk <- pk[,ncol(x = pk),drop = TRUE]
-
-    # pull propensity score corresponding to recommended treatment
-    pidk <- pk*{regime == 1L} + {1.0 - pk}*{regime == 0L}
-
-    return (list("fit" = fitObj, "pidk" = pidk))
-  }
+  #### Propensity Score Regression
 
   fitPS <- list()
-  pid <- matrix(data = 1.0, nrow = n, ncol = K)
+  pid <- rep(x = 1.0, times = n)
 
-  for (k in K:2L) {
+  for (k in 1L:K) {
 
-    # use only individuals with more than 1 tx option
-    one <- data[,txName[k-1L]] == 1L
+    # fit propensity score
+    fitPS[[ k ]] <- modelObj::fit(object = moPS[[ k ]], 
+                                  data = data, 
+                                  response = data[,nms[k]])
 
-    ## kth propensity score regression
+    # product of estimated propensity scores
+    pk <- modelObj::predict(object = fitPS[[ k ]], newdata = data)
+    if (is.matrix(x = pk)) pk <- pk[,ncol(x = pk), drop = TRUE]
 
-    sk <- piStep(moPS = moPS[[ k ]], 
-                 data = data[!one,,drop = FALSE],  
-                 y = data[!one,txName[k]],
-                 regime = regime[!one,k])
-
-    fitPS[[ k ]] <- sk$fit
-    pid[!one,k] <- sk$pidk
+    pid <- pid * {pk*{regime[,k] == 1L} + {1.0 - pk}*{regime[,k] == 0L}}
 
   }
-
-  ## k=1 propensity score regression
-
-  s1 <- piStep(moPS = moPS[[ 1L ]], 
-               data = data,  
-               y = data[,txName[1L]], 
-               regime = regime[,1L])
-
-  fitPS[[ 1L ]] <- s1$fit
-  pid[,1L] <- s1$pidk
-
-  # indicator of all txs received being those recommended
-  cee <- apply(X = {data[,txName] == regime}, MARGIN = 1L, FUN = prod)
-
-  # P(C == 1|H)
-  pid <- apply(X = pid, MARGIN = 1L, FUN = prod)
 
   #### Value Estimate
 
-  value <- mean(x = cee * y / pid)
+  # indicator of tx received was that dictated by the regime
+  Cd <- rowSums(x = regime == data[,nms]) == K
 
-  return( list("fitPS" = fitPS, "valueHat" = value) )
+  value <- mean(x = Cd * y / pid)
+
+  return( list(   "fitPS" = fitPS,
+               "valueHat" = value) )
 }
